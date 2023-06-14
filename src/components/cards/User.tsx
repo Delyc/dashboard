54306c0e9f4aaf3a48b7b9762349302ce896b8b3
delyc <d.twizeyima@alustudent.com>
2023-06-14 17:50:26 +0200

    Feat: Adding options and filter on table

--- src/components/cards/User.tsx
+++ src/components/cards/User.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react"
 import Link from "next/link"
 import styles from '../../styles/Home.module.scss'
 import { ThreeDots } from "../ui/Svgs"
+import Button from "../ui/Button"
 const User = ({ user }: any) => {
     const [show, setShow] = useState(false)
     const [status, setStatus] = useState(user.status)
@@ -36,8 +37,9 @@ const User = ({ user }: any) => {
             <td  className={styles.table__data}>{user.organisationName}</td>
             <td  className={styles.table__data}>{user.username}</td>
             <td className={styles.table__data}>{user.email}</td>
+            <td className={styles.table__data}>{user.phone}</td>
             <td  className={styles.table__data}>{user.dateJoined}</td>
-            <td  style={{ display: "flex", gap: "5px" }} className={styles.table__data}>{status}
+            <td  style={{ display: "flex", gap: "5px" , justifyContent: "space-between"}} className={styles.table__data}>{status}
                 {
                     (user.email === JSON.parse(localStorage.getItem('user')!) || user.username === "delyce") && <td >
                         {
@@ -48,7 +50,9 @@ const User = ({ user }: any) => {
                                 <h1 onClick={(() => onChangeStatus(`${user.status === "active" ? "active" : "inactive"}`))}>{user.status === "active" ? "deActivate User" : "activate User"}</h1>
                             </div>
                         }
-                        <button onClick={editStatus}><ThreeDots /></button></td>
+                        <Button className={styles.table__row_button}  onClick={editStatus} icon={<ThreeDots />}/>
+
+                        </td>
                 }
             </td>
 
--- src/components/sections/UsersTable.tsx
+++ src/components/sections/UsersTable.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import User from "@/components/cards/User";
 import { Filter } from "../ui/Svgs";
-import styles from'../../styles/Home.module.scss'
+import styles from '../../styles/Home.module.scss'
 import Button from "../ui/Button";
 
 const UsersTable = () => {
@@ -25,36 +25,41 @@ const UsersTable = () => {
     }
 
 
-    const tableItem = ["Organiation name", "username", "Email", "DAte Joined", "Status"]
+    const tableItem = ["Organization name", "username", "Email", "phone number", "DAte Joined", "Status"]
 
-    return ( 
+    return (
 
         <table className={styles.table}>
-        <thead className={styles.table__heading}>
-            <tr className={styles.table__row}>
-                {tableItem.map((item: string, index: number) => {
-                    return(
-                        <th className={styles.table__header} key={index}>{item} 
-    
-                        <Button onClick={handleFilter} icon={<Filter />} className={""} /></th>
+            <thead className={styles.table__heading}>
+                <tr className={styles.table__row}>
+                    {tableItem.map((item: string, index: number) => {
+                        return (
+                            <th className={styles.table__header} key={index}>
+
+                                <div className={styles.test}>
+                                    <p className={styles.table__header_name}>{item}
+</p>
+                                    
+                                    <Button onClick={handleFilter} icon={<Filter />} className={styles.table__button} />
+                                </div></th>
+                        )
+                    })}
+
+                </tr>
+            </thead>
+            <tbody className={styles.table__body}>
+
+                {users.map((user: any, index: any) => {
+                    return (
+                        <User key={index} user={user} />
                     )
                 })}
 
-            </tr>
-        </thead>
-        <tbody className={styles.table__body}>
 
-            {users.map((user: any, index: any) => {
-                return (
-                     <User  key={index} user={user}/>
-                )
-            })}
+            </tbody>
+        </table>
 
-
-        </tbody>
-    </table>
-
-     );
+    );
 }
- 
+
 export default UsersTable;
