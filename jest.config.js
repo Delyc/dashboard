3c8870d288b977adf3eb4fae916543cc9519a24d
delyc <d.twizeyima@alustudent.com>
2023-06-16 01:33:48 +0200

    Feat: Adding test coverage

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,23 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+    "!**/coverage/**",
+    "!**/.next/**",
+    "!**/out/**",
+  ],
+  coverageReporters: ["lcov", "text"],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
 };
 module.exports = createJestConfig(customJestConfig);
